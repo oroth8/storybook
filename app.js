e9a756d216ee3b2739cb64bcceaff4276fa082b9
Owen Roth <oroth8@comcast.net>
2020-11-09 20:36:58 -0600

    got public stories to display and work

--- app.js
+++ app.js
@@ -28,9 +28,14 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 // handlebar helper
-const {formatDate} = require("./helpers/hbs")
+const {formatDate, stripTags, truncate, editIcon} = require("./helpers/hbs")
 // handlebars
-app.engine('.hbs', exphbs({helpers: {formatDate}, defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs({helpers: {
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon
+}, defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 // express-session middleware
@@ -44,6 +49,12 @@ app.use(session({
 // passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// global var
+app.use(function(req, res, next){
+    res.locals.user = req.user || null
+    next()
+});
 
 // Static
 app.use(express.static(path.join(__dirname, 'public')));
