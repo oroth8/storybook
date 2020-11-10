8debf5121640a6f4f4e81055de3eeea7d6f37f6a
Owen Roth <oroth8@comcast.net>
2020-11-09 20:21:29 -0600

    added add hbs and index hbs

--- app.js
+++ app.js
@@ -19,12 +19,18 @@ connectDB();
 
 const app = express();
 
+// body Parser
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 // Logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
+// handlebar helper
+const {formatDate} = require("./helpers/hbs")
 // handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs({helpers: {formatDate}, defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 // express-session middleware
